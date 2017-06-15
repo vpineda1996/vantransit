44c828e1b9da86816ccfcae68e63be3d87278c33
Victor Pineda Gonzalez <vpineda1996@gmail.com>
2017-06-14 23:42:24 -0700

    Initial commit

---
+++ src/js/translink.js
@@ -0,0 +1,45 @@
+var method = 'GET';
+var api = (function(){
+    var url = "http://api.translink.ca/rttiapi/v1/"
+    var apiKey = '0GKoVlC6TgpNJJNRjZ1r'
+    function getWithApiKey(url, routes, fields) {
+        fields["apiKey"] = apiKey
+        return url + routes.join("/") + "?" + Object.keys(fields).map(function(arg){ return arg + "=" + fields[arg]}).join("&")
+    }
+    return {
+        stops: url + "stops/",
+        buses: url + "buses/",
+        route: url + "routes/",
+        getWithApiKey: getWithApiKey
+    }
+})()
+
+
+function getStops(lat, long, radius) {
+    var url = api.getWithApiKey(api.stops, [], {
+        lat: lat,
+        long: long,
+        radius: radius
+    });
+    request(url, parseStopsXML, parseStopsXML)
+}
+
+function parseStopsXML(xmlRes) {
+    var xmlDoc = xmlRes.responseXML;
+    console.log(xmlRes.responseText)
+}
+
+function request(url, callbackSuccess, callbackFail) {
+    var request = new XMLHttpRequest();
+    request.onload = function() {
+        if(this.status == 200) callbackSuccess(this)
+        else callbackFail(this)
+    };
+    request.open(method, url);
+    request.send();
+}
+
+module.exports = {
+    echo: 'echo',
+    getStops: getStops
+}
